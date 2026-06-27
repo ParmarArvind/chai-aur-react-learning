bebe3bbef544174323801f5218054e9fdf385666
Arvind Parmar <arvind1057956@gmail.com>
2026-06-28 02:40:27 +0530

    Add Day 1 - Basic React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from './chai.jsx'
+function App() {
+  return (
+    <>
+    <h1>Hello, React!</h1>
+    <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/chai.jsx
@@ -0,0 +1,8 @@
+// it is a component , always start with capital letter 
+//best practice to make file name starts with capital letter
+function Chai(){
+    return (
+        <h3>Chai is ready in basic react</h3>
+    )
+}
+export default Chai
